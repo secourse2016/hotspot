4178f07ec792a7f5bf6d757d7f899a830d876761
Ahmed-anwar <ahmedanwarm@gmail.com>
2016-04-04 13:12:46 +0200

    started working on flight views

--- public/core.js
+++ public/core.js
@@ -6,19 +6,17 @@ App = angular.module('Swiss', ['ui.bootstrap', 'ngRoute']); //'ngAnimate'
  */
 App.config(function($routeProvider) {
     $routeProvider
-
         // route for the home page
         .when('/', {
             templateUrl : '/partials/main.html',
             controller  : 'mainCtrl'
         })
-
         // route for the flights page
         .when('/flights', {
             templateUrl : '/partials/flights.html',
             controller  : 'flightsCtrl'
         })
-
+        
         .when('/confirmation', {
             templateUrl : '/partials/confirmation.html',
             controller  : 'confirmationCtrl'
