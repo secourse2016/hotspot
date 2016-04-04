bb7b28d2b7b32e8dfa6290e2d0f3f8b8af0d3eb2
moustafa-nagaty <moustafro@hotmail.com>
2016-04-04 05:33:56 +0200

    finalized confirmation

--- public/controllers/confirmationCtrl.js
+++ public/controllers/confirmationCtrl.js
@@ -1,4 +1,14 @@
 
-App.controller('confirmationCtrl',function($scope, PaymentSrv) {
+App.controller('confirmationCtrl',function($scope, PaymentSrv, $location) {
   $scope.user = PaymentSrv.getUser();
+
+  $scope.GoToMain = function() {
+    // window.alert("Thank you for your purchase. Your ticket will be e-mailed to you shortly.");
+    $location.url('/');
+  };
+
+  $scope.EditInfo = function() {
+    $location.url('/payment');
+  };
+
 });
