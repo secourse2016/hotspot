8495a96b05decc3794c5349736ffe1d840809d4c
Ahmed-anwar <ahmedanwarm@gmail.com>
2016-04-17 18:24:41 +0200

    removed function submitform

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -53,14 +53,7 @@ App.controller('mainCtrl', function($scope, FlightsSrv, $location) {
         $location.url('/flights');
   };
 
-  $scope.submitForm = function(isValid) {
 
-    // check to make sure the form is completely valid
-    if (isValid) {
-      alert('our form is amazing');
-    }
-
-  };
 
   /* Get Airports on page render  */
   AirportCodes();
