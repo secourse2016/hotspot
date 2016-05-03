0c62b11b062b52464d47403d2886c7dee40c171f
Ezzo1995 <ahmed_sherif95@hotmail.com>
2016-05-03 23:18:23 +0200

    added bookingS2 routing

--- www/js/app.js
+++ www/js/app.js
@@ -76,7 +76,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'paymentCtrl'
       }
     }
-  });
+  })
+.state('bookingS2', {
+    url: "/bookingS2",
+    templateUrl: 'templates/bookingS2.html'
+});
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
 
