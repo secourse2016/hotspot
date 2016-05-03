a4651f7e698f1cc359cf5b8eab22e473d79552cd
ahmedkhaled95 <ahmed.1611@hotmail.com>
2016-05-03 19:26:50 +0200

    updated app.js fro payment routing

--- www/js/app.js
+++ www/js/app.js
@@ -67,8 +67,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'aboutCtrl'
       }
     }
+  })
+.state('payment', {
+    url: '/book/:tab-payment',
+    views: {
+      'tab-payment': {
+        templateUrl: 'templates/tab-payment.html',
+        controller: 'paymentCtrl'
+      }
+    }
   });
-
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
 
