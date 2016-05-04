6201b321f74f68f2e2c176c7f64092f4179f37ab
omarkamaal <omar.kamal-ghanem@student.guc.edu.eg>
2016-05-03 20:35:37 -0400

    Added binding variables to tab-html and updated ctrl.. Medo mota2allek el sprintaya de

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,14 +38,32 @@ angular.module('starter.controllers',[])
   $scope.SetOtherAirlines= function(otherAirlines) {
    FlightsSrv.setSelectedAirlines(otherAirlines);
  };
+<<<<<<< HEAD
+ $scope.SetOriginAirport= function(orgAirport) {
+   FlightsSrv.setSelectedOriginAirport(orgAirport);
+ };
+   $scope.SetClass = function(cls) {
+    FlightsSrv.setSelectedClass(cls);
+  };
+=======
+>>>>>>> e27de72a71ecfa41967fb8cbfb7e042e96258cc3
 
   /* Find All Available Flights  */
   $scope.SearchFlights = function() {
         $scope.SetOutgoingDate($scope.outDate);
         $scope.SetIncomingDate($scope.inDate);
+<<<<<<< HEAD
+        $scope.SetRoundTrip($scope.twoWay);
+        $scope.SetOtherAirlines($scope.otherAirlines);
+        $scope.SetDestinationAirport($scope.destAirport);
+        $scope.SetOriginAirport($scope.orgAirport);
+        $scope.setSelectedClas($scope.cls);
+       // $state.go(''); once booking stage 2 is done and complete, we should change state to it. It will now be ready to get files by an http request.
+=======
         $scope.SetRoundTrip($scope.roundTrip);
         $scope.SetOtherAirlines($scope.otherAirlines);
         
+>>>>>>> e27de72a71ecfa41967fb8cbfb7e042e96258cc3
   };
 
 }])
